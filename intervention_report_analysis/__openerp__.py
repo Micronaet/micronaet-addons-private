4a8c307105835a620ba7a67d7609215123ea7b35
nicothebrush <nicola.riolini@gmail.com>
2015-04-09 18:41:23 +0200

    Start work on report for intervention analysis

---
+++ intervention_report_analysis/__openerp__.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+# OpenERP, Open Source Management Solution
+# Copyright (C) 2001-2015 Micronaet S.r.l. (<http://www.micronaet.it>)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+
+
+{
+    'name': 'Intervention analysis',
+    'version': '0.1',
+    'category': 'Report',
+    'description': """
+        Add extra element for analysis in intervention report
+        
+        """,
+    'author': 'Micronaet s.r.l.',
+    'website': 'http://www.micronaet.it',
+    'license': 'AGPL-3',
+    'depends': [
+        'base',
+        'intervention_report',
+        'report_webkit',
+        ],
+    'init_xml': [],
+    'demo_xml': [],
+    'update_xml': [
+        #'security/ir.model.access.csv',
+        'analysys_view.xml',  
+        'report/status_webkit.xml',
+        #'wizard/wizard_create_trip_view.xml',                    
+        ],
+    'active': False,
+    'installable': True,
+    }
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
