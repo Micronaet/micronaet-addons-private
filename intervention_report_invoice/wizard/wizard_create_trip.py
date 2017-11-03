91bba1f5178325215462b552a257f52cd5620c95
nicothebrush <nicola.riolini@gmail.com>
2017-11-03 18:28:15 +0100

    extract in xlsx intervent

--- intervention_report_invoice/wizard/wizard_create_trip.py
+++ intervention_report_invoice/wizard/wizard_create_trip.py
@@ -54,7 +54,7 @@ class account_invoice_intervent_wizard(osv.osv_memory):
     # -------------------------------------------------------------------------
     # Utility:    
     # -------------------------------------------------------------------------
-    def get_xls_format(self, mode, WB=None, num_format='#,##0'):  
+    def get_xls_format(self, mode, WB=None, num_format='#,##0.00'):  
         ''' Database for format cells
             first call is with mode not present and WB pased
             next call with only mode
@@ -472,6 +472,14 @@ class account_invoice_intervent_wizard(osv.osv_memory):
     def create_intervent_list(self, cr, uid, ids, context=None):
         ''' Create list of intervent depend on selection
         ''' 
+        # Parameters:
+        smb_root = '~/filestore/samba/intervent'
+        smb_root = os.path.expanduser(smb_root)
+        try:
+            os.system('mkdir -p %s' % smb_root)
+        except:
+            _logger.error('Cannot create folder: %s' % smb_root)    
+        
         # Pool used:
         intervent_pool = self.pool.get('hr.analytic.timesheet')
         invoice_pool = self.pool.get('account.invoice')    
@@ -483,6 +491,7 @@ class account_invoice_intervent_wizard(osv.osv_memory):
         month = wiz_proxy.month
         invoice_id = wiz_proxy.invoice_id.id
         #user_id = wiz_proxy.user_id.id
+        #mode = wiz_proxy.mode
 
         if invoice_id:
             invoice_ids = [invoice_id]                
@@ -498,19 +507,14 @@ class account_invoice_intervent_wizard(osv.osv_memory):
             # -----------------------------------------------------------------
             # Open file XSLX:
             # -----------------------------------------------------------------
-            name = invoice.partner_id.name if invoice.partner_id else 'Manca'
-            filename = '~/Scrivania/list/intervent_%s_%s_%s.xlsx' % (
-                wiz_proxy.year,
-                wiz_proxy.month,
+            name = invoice.partner_id.name if invoice.partner_id else 'Manca'            
+            filename = os.path.join(smb_root, 'Interventi_%s_%s_%s.xlsx' % (
+                year,
+                month,
                 name,
-                )
-            filename = os.path.expanduser(filename)    
+                ))
             
             WB = xlsxwriter.Workbook(filename)
-            WS = WB.add_worksheet('Interventi %s-%s' % (
-                wiz_proxy.year,
-                wiz_proxy.month,
-                ))
 
             # Format used:                
             format_title = self.get_xls_format('title', WB)
@@ -522,24 +526,47 @@ class account_invoice_intervent_wizard(osv.osv_memory):
             # -----------------------------------------------------------------
             # 0. Set column dimension:
             # -----------------------------------------------------------------
+            # Sheet Intervent:
+            WS = WB.add_worksheet('Interventi %s-%s' % (year, month))
             WS.set_column('A:A', 30)
             WS.set_column('C:C', 20)
             WS.set_column('D:D', 30)
-            # TODO 
-            #WS.merge_range(row, 0, row, 12, '') # merge cell
+            WS.set_column('E:E', 15)
+            WS.set_column('F:F', 30)
+            WS.set_column('G:G', 15)
+            WS.set_column('H:H', 15)
+            
+            # Sheet Contract:
+            WS_c = WB.add_worksheet('Contratti %s-%s' % (year, month))
+            WS_c.set_column('A:A', 30)
+            WS_c.set_column('B:B', 10)
+            WS_c.set_column('C:C', 20)
+            WS_c.set_column('D:D', 10)
+            WS_c.set_column('E:E', 10)
+            WS_c.set_column('F:F', 10)
+            
+            # XXX WS.merge_range(row, 0, row, 12, '') # merge cell
 
             # -----------------------------------------------------------------
             # 1. Partner header:
             # -----------------------------------------------------------------
             #WS.write_rich_string(row, col, *record)
+            # Sheet Intervent:
             self.write_xlsx_line(
                 WS, 0, [
-                    'Partner: %s' % name,
+                    'Interventi (dettaglio): %s' % name,
+                    ], format_title)
+
+            # Sheet Contract:
+            self.write_xlsx_line(
+                WS_c, 0, [
+                    'Contratti (sommario): %s' % name,
                     ], format_title)
             
             # -----------------------------------------------------------------
             # 2. Header title:
             # -----------------------------------------------------------------
+            # Sheet Intervent:
             self.write_xlsx_line(
                 WS, 2, [
                     u'Commessa',
@@ -549,23 +576,36 @@ class account_invoice_intervent_wizard(osv.osv_memory):
                     u'Tecnico',
                     u'Oggetto', 
                     u'Modalità',
+                    u'Fatturabile',
                     u'Traferta',
                     u'Pausa',
                     u'Effettivo',
                     u'Fatturato',            
                     ], format_header)
-            
+
+            # Sheet Contract:
+            self.write_xlsx_line(
+                WS_c, 2, [
+                    u'Commessa',
+                    u'Num.',
+                    u'Responsabile', 
+                    u'Dalla data',
+                    u'Alla data',
+                    u'Fatturato',
+                    ], format_header)
+                        
             # -----------------------------------------------------------------
             # 3. Table: list of intervent
             # -----------------------------------------------------------------
             row = 2
-            
+            res = {}
             for intervent in invoice.intervention_report_ids:
                 row += 1
                 request = intervent.intervention_request or ''
+                total = intervent_pool.get_total_h_2_invoice(
+                    intervent)
                 if request == 'Nuovo evento':
                     request = intevent.name
-                    
                 self.write_xlsx_line(
                     WS, row, [
                         intervent.account_id.name,
@@ -575,15 +615,32 @@ class account_invoice_intervent_wizard(osv.osv_memory):
                         intervent.user_id.name,
                         request,
                         (intervent.mode, format_center),
+                        (intervent.to_invoice.name, format_center),
                         (intervent.trip_hour if intervent.trip_require \
                             else '/', format_number),
                         (intervent.break_hour if intervent.break_require \
                             else '/', format_number),
                         (intervent.intervent_total, format_number),
-                        (intervent_pool.get_total_h_2_invoice(
-                            intervent), format_number),  
+                        (total, format_number),  
                         ], format_text)
-              
+                if intervent.account_id not in res:
+                    res[intervent.account_id] = total
+                else:    
+                    res[intervent.account_id] += total
+                        
+            # Summary mode need write after total:                    
+            row = 2
+            for account in sorted(res, key=lambda x: x.name):
+                row += 1
+                self.write_xlsx_line(
+                    WS_c, row, [
+                        account.name,
+                        account.code,
+                        account.manager_id.name or '',
+                        account.from_date or '',
+                        account.to_date or '',
+                        (res[account], format_number),
+                        ], format_text)                 
             # End operations:
             WB.close()
             
