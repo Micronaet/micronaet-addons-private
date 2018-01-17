54866e606f37f03e1a3de6f309275c6f18f6fa44
nicothebrush <nicola.riolini@gmail.com>
2018-01-17 17:55:02 +0100

    generic bugfix (fast message)

--- intervention_report_analysis/wizard/extract_personal_stats_wizard.py
+++ intervention_report_analysis/wizard/extract_personal_stats_wizard.py
@@ -214,6 +214,7 @@ class AccountDistributionStatsWizard(orm.TransientModel):
         # ---------------------------------------------------------------------
         # Layout setup:        
         excel_pool.column_width(WS_name, [25, 40, 10, 10, 10, 10])
+        import pdb; pdb.set_trace()
         f_title = excel_pool.get_format('title')
         f_header = excel_pool.get_format('header')
 
@@ -269,7 +270,7 @@ class AccountDistributionStatsWizard(orm.TransientModel):
                 widget_float_time(data[2], float_time), 
                 widget_float_time(data[3], float_time), 
                 ])
-                
+        import pdb; pdb.set_trace()        
         return excel_pool.return_attachment(
             cr, uid, 'Statistiche', version='7.0', 
             context=context)
