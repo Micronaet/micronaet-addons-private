b0301c2c87901bd6aae66c6760a9c590a530421f
nicothebrush <nicola.riolini@gmail.com>
2018-01-17 15:07:54 +0100

    generic bugfix (fast message)

--- intervention_report_analysis/wizard/extract_personal_stats_wizard.py
+++ intervention_report_analysis/wizard/extract_personal_stats_wizard.py
@@ -147,7 +147,7 @@ class AccountDistributionStatsWizard(orm.TransientModel):
             for account in account_pool.browse(
                     cr, uid, account_ids, context=context):
                 # for user filter check if user is in distribution:
-                todo = self.get_account_distribution(
+                todo = account_pool.get_account_distribution(
                     user_id, from_date, to_date, account)
                 if user_id and user_id not in [
                         item.id for item in account.distribution_ids]:
