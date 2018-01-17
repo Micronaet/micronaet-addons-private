c5477cd26df584d0879a595f2aaaf58750fa6097
nicothebrush <nicola.riolini@gmail.com>
2018-01-17 16:59:27 +0100

    generic bugfix (fast message)

--- intervention_report_analysis/wizard/extract_personal_stats_wizard.py
+++ intervention_report_analysis/wizard/extract_personal_stats_wizard.py
@@ -284,7 +284,7 @@ class AccountDistributionStatsWizard(orm.TransientModel):
         
     _defaults = {
         'contract': lambda *x: True,
-        'float_hour': lambda *x: True,
+        'float_time': lambda *x: True,
         'user_id': lambda s, cr, uid, ctx: uid,
         'from_date': lambda *x: datetime.now().strftime('%Y-%m-01'),
         'to_date': lambda *x: (
