dae36b91d90d5654e54e52c1cdbda814da7ae333
nicothebrush <nicola.riolini@gmail.com>
2018-01-17 17:06:23 +0100

    add is extra report param

--- intervention_report/intervention.py
+++ intervention_report/intervention.py
@@ -28,8 +28,10 @@ class account_analytic_account(osv.osv):
     _inherit = 'account.analytic.account'
     
     _columns = {
-        'default_to_invoice': fields.many2one('hr_timesheet_invoice.factor', 'Default invoice', help = "Defaulf invoice type if there's one active for customer. All intervent are, for default, setted to this value"),
+        'default_to_invoice': fields.many2one('hr_timesheet_invoice.factor', 'Default invoice', help = "Defaulf invoice type if there's one active for customer. All intervent are, for default, setted to this value"),        
         'total_hours': fields.float('Total hour', digits = (16, 2), help = "Total hour for this contract for all period"),        
+        'is_extra_report': fields.boolean('Extra report', 
+            help='Hided in reports'),
         #'request_by':fields.char('Request by', size=100, 
         #    help='List of people that request intervent'
         #    ),
