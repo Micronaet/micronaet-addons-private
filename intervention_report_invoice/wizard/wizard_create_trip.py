13a944417232f8e429394500982c15614d8726d2
nicothebrush <nicola.riolini@gmail.com>
2023-03-06 16:23:05 +0100

    generic bugfix (fast message)

--- intervention_report_invoice/wizard/wizard_create_trip.py
+++ intervention_report_invoice/wizard/wizard_create_trip.py
@@ -599,7 +599,7 @@ class account_invoice_intervent_wizard(osv.osv_memory):
             result = base64.decodestring(report['result'])
 
             # Generate file:
-            filename = 'Interventi_%s-%s_%s.odt' % (
+            filename = 'Interventi_%s-%s_%s.pdf' % (
                 month,
                 year,
                 self.clean_for_file(invoice.partner_id.name),
