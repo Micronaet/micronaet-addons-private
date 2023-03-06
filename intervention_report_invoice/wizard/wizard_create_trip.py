af165dd0b3ed3f9ce53f479ffb05d96d30476636
nicothebrush <nicola.riolini@gmail.com>
2023-03-06 16:57:53 +0100

    generic bugfix (fast message)

--- intervention_report_invoice/wizard/wizard_create_trip.py
+++ intervention_report_invoice/wizard/wizard_create_trip.py
@@ -603,7 +603,7 @@ class account_invoice_intervent_wizard(osv.osv_memory):
                 self.clean_for_file(partner.name),
             )
             _logger.info('Generating: [%s] %s...' % (
-                partner.name
+                partner.name,
                 filename,
             )
 
