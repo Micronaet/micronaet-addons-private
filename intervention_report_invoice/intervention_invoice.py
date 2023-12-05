21e398cb02c25bb297424af32f0a66b3d34b0af2
nicothebrush <nicola.riolini@gmail.com>
2023-12-05 16:54:43 +0100

    generic bugfix (fast message)

--- intervention_report_invoice/intervention_invoice.py
+++ intervention_report_invoice/intervention_invoice.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-################################################################################
+###############################################################################
 #
-#    OpenERP, Open Source Management Solution    
+#    OpenERP, Open Source Management Solution
 #    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>). All Rights Reserved
 #    d$
 #
@@ -18,18 +18,19 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-################################################################################
+###############################################################################
 
 from osv import osv, fields
 
+
 class hr_analytic_timesheet(osv.osv):
-    ''' Extra fields for account.invoice
-    '''
+    """ Extra fields for account.invoice
+    """
     _inherit = 'hr.analytic.timesheet'
-    
+
     def get_total_h_2_invoice(self, intervent, no_factor=False):
-        ''' Calculate total for intevent: 
-        '''
+        """ Calculate total for intevent:
+        """
         if no_factor:
             factor = 1.0
         else:
@@ -44,61 +45,79 @@ class hr_analytic_timesheet(osv.osv):
                 intervent.intervent_duration + trip_h - break_h)
         return total
 
+
 class account_invoice(osv.osv):
-    ''' Extra fields for account.invoice
-    '''
-    _inherit='account.invoice'
-    
-    def _function_summary_intervent(self, cr, uid, ids, fields, param, context=None):
-        ''' In all intervention_report_ids compute:
+    """ Extra fields for account.invoice
+    """
+    _inherit = 'account.invoice'
+
+    def _function_summary_intervent(
+            self, cr, uid, ids, fields, param, context=None):
+        """ In all intervention_report_ids compute:
             total to_invoice
             Description for group of intervent
-        '''
-        res={}
+        """
+        res = {}
         for invoice in self.browse(cr, uid, ids, context=context):
-            res[invoice.id]={}
-            res[invoice.id]['to_invoice_total'] = 0.0            
+            res[invoice.id] = {}
+            res[invoice.id]['to_invoice_total'] = 0.0
             res[invoice.id]['to_invoice_summary'] = ""
-            summary={}
-            for intervent in invoice.intervention_report_ids:                
+            summary = {}
+            for intervent in invoice.intervention_report_ids:
                 if intervent.state == 'cancel':
                     continue
-                
-                if intervent.manual_total: # Manual intervent:
-                    total_hours=intervent.intervent_total or 0.0
-                else:                      # Calculate with duration, trip, break
-                    total_hours=((intervent.intervent_duration) + (intervent.trip_hour if intervent.trip_require else 0.0) - (intervent.break_hour if intervent.break_require else 0.0))
 
-                if intervent.not_in_report: # not present in report                 
-                    summary_name="Not in report"
-                else: 
-                    summary_name=intervent.to_invoice.name
-                    res[invoice.id]['to_invoice_total'] += (intervent.to_invoice.factor or 0.0) / 100 * total_hours # only if in report (total)
+                partnership_mode = intervent.partnership_mode or 'Azienda'
 
-                if summary_name not in summary: # Summary with also not in report values
-                    summary[summary_name] = total_hours or 0.0
-                else:    
-                    summary[summary_name] += total_hours or 0.0
-            res[invoice.id]['to_invoice_total_price'] = res[invoice.id]['to_invoice_total'] * (intervent.intervent_partner_id.hour_cost if intervent.intervent_partner_id and intervent.intervent_partner_id.hour_cost else 0.0)
+                # Manual intervent:
+                if intervent.manual_total:
+                    total_hours = intervent.intervent_total or 0.0
+                else:   # Calculate with duration, trip, break
+                    total_hours = (
+                        intervent.intervent_duration +
+                        (intervent.trip_hour if intervent.trip_require
+                         else 0.0) -
+                        (intervent.break_hour if intervent.break_require
+                         else 0.0))
+
+                if intervent.not_in_report:  # not present in report
+                    summary_name = 'Escluso dal report'
+                else:
+                    summary_name = intervent.to_invoice.name
+                    res[invoice.id]['to_invoice_total'] += \
+                        (intervent.to_invoice.factor or 0.0) / 100 * \
+                        total_hours  # only if in report (total)
+
+                # Summary with also not in report values
+                if summary_name not in summary:
+                    summary[summary_name] = total_hours
+                else:
+                    summary[summary_name] += total_hours
+
+            hour_cost = intervent.intervent_partner_id.hour_cost or 0.0
+            res[invoice.id]['to_invoice_total_price'] = \
+                res[invoice.id]['to_invoice_total'] * hour_cost
             for key in summary.keys():
-                res[invoice.id]['to_invoice_summary'] += "%s: %s\n"%(key, summary[key])                
+                res[invoice.id]['to_invoice_summary'] += '%s: %s\n' % (
+                    key, summary[key])
         return res
-        
+
     _columns = {
-        'intervention_report_ids':fields.one2many('hr.analytic.timesheet', 
+        'intervention_report_ids': fields.one2many(
+            'hr.analytic.timesheet',
             'invoice_id', 'Intervents'),
-        
+
         'to_invoice_total': fields.function(
-            _function_summary_intervent, method=True, type='float', 
-            digits=(16, 2), string='Total to invoice (h)', store=False, 
+            _function_summary_intervent, method=True, type='float',
+            digits=(16, 2), string='Total to invoice (h)', store=False,
             multi=True),
         'to_invoice_total_price': fields.function(
-            _function_summary_intervent, method=True, type='float', 
-            digits=(16, 2), string='Total to invoice (€)', 
+            _function_summary_intervent, method=True, type='float',
+            digits=(16, 2), string='Total to invoice (€)',
             store=False, multi=True),
-        'to_invoice_summary': fields.function(_function_summary_intervent, 
-            method=True, type='text', string='Summary', store=False, 
+        'to_invoice_summary': fields.function(
+            _function_summary_intervent,
+            method=True, type='text', string='Summary', store=False,
             multi=True),
         'comment': fields.text('Internal comment'),
         }
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
