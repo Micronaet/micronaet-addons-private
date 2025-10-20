f8b2a6ee3648655bbc3897cf0355e2483e9e8997
NicoTheBrush <nicola.riolini@gmail.com>
2025-10-20 15:46:23 +0200

    Fast commit

--- partner_mail_manage/email.py
+++ partner_mail_manage/email.py
@@ -20,6 +20,8 @@
 import os
 import sys
 import logging
+import requests
+import json
 from osv import osv, orm, fields
 from datetime import datetime, timedelta
 from openerp.tools import (
@@ -34,6 +36,221 @@ class ResPartnerEmailServer(orm.Model):
     """
     _name = 'res.partner.email.server'
     _description = 'Mail Server'
+
+    def button_load_all_domain_data(self, cr, uid, ids, context=None):
+        """ Load all data in this server
+        """
+        # --------------------------------------------------------------------------------------------------------------
+        #                                               Utility:
+        # --------------------------------------------------------------------------------------------------------------
+        def get_data_with_token(url, headers, mode='get', verbose=False):
+            """ Call Endpoint passing headers data
+            """
+            if verbose:
+                _logger.info('Connecting to {} ...'.format(url))
+
+            try:
+                if mode == 'get':
+                    response = requests.get(url, headers=headers)
+                elif mode == 'post':
+                    response = requests.get(url, headers=headers)
+                else:
+                    _logger.info('No mode {}'.format(mode))
+                    return False
+
+                response.raise_for_status()
+                if verbose:
+                    print('Connected! Status code {}'.format(response.status_code))
+                return response.json()
+
+            except requests.exceptions.HTTPError as errh:
+                print('Error HTTP: {}'.format(errh))
+                print('Reply response: {}'.format(response.text))
+            except requests.exceptions.ConnectionError as errc:
+                print('Connection error: {}'.format(errc))
+            except requests.exceptions.Timeout as errt:
+                print('Timeout in request: {}'.format(errt))
+            except requests.exceptions.RequestException as err:
+                print('Generic error: {}'.format(err))
+            return None
+
+        # --------------------------------------------------------------------------------------------------------------
+        #                                                      Procedure:
+        # --------------------------------------------------------------------------------------------------------------
+        domain_pool = self.pool.get('res.partner.email.domain')
+        email_pool = self.pool.get('res.partner.email.domain.mail')
+        alias_pool = self.pool.get('res.partner.email.domain.alias')
+
+        server_id = ids[0]
+        imap_server = self.browse(cr, uid, server_id, context=context)
+
+        ROOT_URL = imap_server.root_url
+        API_TOKEN = imap_server.token
+
+        HEADERS = {
+            "X-API-Token": API_TOKEN,
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+        }
+
+        # --------------------------------------------------------------------------------------------------------------
+        # Load Domain list:
+        # --------------------------------------------------------------------------------------------------------------
+        domain_url = '{}/domains'.format(ROOT_URL)
+        domain_reply = get_data_with_token(domain_url, HEADERS, mode='post')
+
+        domain_data = {}
+
+        # Loop to read Domain extra data:
+        loop = [
+            ('email', 'email_accounts'),
+            ('alias', 'alias_email_accounts'),
+        ]
+
+        if domain_reply:
+            for domain in domain_reply['resources']:
+
+                # ------------------------------------------------------------------------------------------------------
+                # Load domain emails:
+                # ------------------------------------------------------------------------------------------------------
+                domain_code = domain['code']
+                domain_name = domain['name']
+                _logger.info('\n\nReading Domain: {}'.format(domain_name))
+
+                domain_data[domain_name] = {
+                    'detail': domain,
+                    'email': [],
+                    'alias': [],
+                }
+
+                for key, endpoint in loop:
+                    email_url = '{}/domains/{}/{}'.format(ROOT_URL, domain_code, endpoint)
+                    email_payload = HEADERS.copy()
+                    page = 1
+
+                    while True:
+                        email_payload['page'] = page
+                        reply = get_data_with_token(email_url, HEADERS, mode='get')
+                        if reply:
+                            for email in reply['resources']:
+                                domain_data[domain_name][key].append(email)
+
+                            total_pages = reply['pagination']['total_pages']
+                            page += 1
+                            if page > total_pages:
+                                print('Max page raised {}'.format(total_pages))
+                                break
+                            else:
+                                print('Read next page {} of {}'.format(page, total_pages))
+                        else:
+                            print('No mail for domain {}'.format(domain_code))
+                            break
+
+        else:
+            _logger.error('Cannot connect to API server')
+            return False
+
+        # --------------------------------------------------------------------------------------------------------------
+        # Update data
+        # --------------------------------------------------------------------------------------------------------------
+        previous_domain_ids = domain_pool.search(cr, uid, [
+            ('server_id', '=', server_id)
+        ], context=context)
+
+        previous_email_ids = email_pool.search(cr, uid, [
+            ('domain_id.server_id', '=', server_id),
+        ], context=context)
+
+        previous_alias_ids = alias_pool.search(cr, uid, [
+            ('domain_id.server_id', '=', server_id),
+        ], context=context)
+
+        for domain_name in domain_data:
+
+            # ----------------------------------------------------------------------------------------------------------
+            # Domain update:
+            # ----------------------------------------------------------------------------------------------------------
+            domain_record = domain_data[domain_name]['detail']
+            domain_code = domain_record['code']
+            _logger.info('Update domain: {}'.format(domain_name))
+            this_domain_ids = domain_pool.search(cr, uid, [
+                ('server_id', '=', server_id),
+                ('code', '=', domain_code),
+            ], context=context)
+
+            if this_domain_ids:
+                # No update for now
+                this_domain_id = this_domain_ids[0]
+            else:
+                this_domain_id = domain_pool.create(cr, uid, {
+                    'code': domain_code,
+                    'name': domain_record['code'],
+                    'server_id': server_id,
+                }, context=context)
+
+            previous_domain_ids.remove(this_domain_id)
+
+            # ----------------------------------------------------------------------------------------------------------
+            # Mail update:
+            # ----------------------------------------------------------------------------------------------------------
+            for email in domain_data[domain_name]['email']:
+                this_email_ids = email_pool.search(cr, uid, [
+                    ('domain_id', '=', this_domain_id),
+                    ('name', '=', email['email_address'])
+                ], context=context)
+
+                data = {
+                    'name': email['email_address'],
+                    # 'create_date': email['created_at'],
+                    'dimension': email['max_email_quota'] / (1024 ** 3),
+                    'active': email['status'] == 'enabled',
+                }
+                if this_email_ids:
+                    email_pool.write(cr, uid, this_email_ids, data, context=context)
+                    this_email_id = this_email_ids[0]
+                else:
+                    this_email_id = email_pool.create(cr, uid, data, context=context)
+                previous_email_ids.remove(this_email_id)
+
+            for alias in domain_data[domain_name]['alias']:
+                alias_name = '{}@{}'.format(alias['name'], domain_name),
+                this_alias_ids = alias_pool.search(cr, uid, [
+                    ('domain_id', '=', this_domain_id),
+                    ('name', '=', alias_name),
+                ], context=context)
+
+                data = {
+                    'name': alias_name,
+                    # 'create_date': alias['created_at'],
+                    'redirect': ', '.join(alias['destinations']),
+                    'active': alias['status'] == 'enabled',
+                }
+                if this_alias_ids:
+                    alias_pool.write(cr, uid, this_alias_ids, data, context=context)
+                    this_alias_id = this_alias_ids[0]
+                else:
+                    this_alias_id = alias_pool.create(cr, uid, data, context=context)
+                previous_alias_ids.remove(this_alias_id)
+
+            # ----------------------------------------------------------------------------------------------------------
+            # Removed data operation:
+            # ----------------------------------------------------------------------------------------------------------
+            if previous_domain_ids:
+                _logger.info('Removed domain {}'.format(previous_domain_ids))
+                domain_pool.write(cr, uid, previous_domain_ids, {
+                    'removed': True,
+                }, context=context)
+            if previous_email_ids:
+                _logger.info('Removed email {}'.format(previous_email_ids))
+                email_pool.write(cr, uid, previous_email_ids, {
+                    'removed': True,
+                }, context=context)
+            if previous_alias_ids:
+                _logger.info('Removed alias {}'.format(previous_alias_ids))
+                alias_pool.write(cr, uid, previous_alias_ids, {
+                    'removed': True,
+                }, context=context)
+        return True
 
     _columns = {
         'token': fields.char('API Token', size=60, required=True),
@@ -55,6 +272,7 @@ class ResPartnerEmailDomain(orm.Model):
         'code': fields.char('Codice', size=30, required=True),
         'name': fields.char('Dominio', size=30, required=True),
         'partner_id': fields.many2one('res.partner', 'Partner'),
+        'removed': fields.boolean('Rimosso', help='Dominio cancellata, non più presente'),
         'note': fields.text('Note'),
          }
 
